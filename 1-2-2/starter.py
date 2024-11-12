f696f2b51c2c61be35bea45558993b1c27586ef5
mrkiedes <akiedes@vtsd.com>
2024-11-12 11:51:20 -0500

    1-2-2 Step 28 Update

--- 1-2-2/starter.py
+++ 1-2-2/starter.py
@@ -8,7 +8,6 @@
 # -----import statements-----
 import turtle as trtl
 import random as rand
-import leaderboard as lb
 
 # -----game configuration-----
 # To view in trinket change the values of font_size, spot_size, and
@@ -22,8 +21,6 @@ timer = 30
 counter_interval = 1000
 timer_up = False
 score = 0
-leaderboard_file_name = "a122_leaderboard.txt"
-player_name = input("What is your name?")
 
 # -----initialize the turtles-----
 spot = trtl.Turtle()
@@ -112,25 +109,6 @@ def start_game():
     spot.onclick(spot_clicked)
     counter.getscreen().ontimer(countdown, counter_interval)
 
-# Add this function to your game code
-
-# manages the leaderboard for top 5 scorers
-def manage_leaderboard():
-
-  global score
-  global spot
-
-  # get the names and scores from the leaderboard file
-  leader_names_list = lb.get_names(leaderboard_file_name)
-  leader_scores_list = lb.get_scores(leaderboard_file_name)
-
-  # show the leaderboard with or without the current player
-  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
-    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
-    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, spot, score)
-
-  else:
-    lb.draw_leaderboard(False, leader_names_list, leader_scores_list, spot, score)
 
 # ----------events----------
 start_game()
