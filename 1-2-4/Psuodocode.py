6390d68c9641dceb695f1ee820982c1d4a167116
15608 <zachary.geraghty@vtsd.com>
2024-11-26 13:57:41 -0500

    2-1-4 Step 8

--- 1-2-4/Psuodocode.py
+++ 1-2-4/Psuodocode.py
@@ -1,13 +1,27 @@
-import turtle as trtl
+import turtle as maze_painter
+
+num_of_walls = 25
+path_width = 10
+wall_color = "black"
+wall_length = 10
+maze_painter.pencolor(wall_color)
+maze_painter.hideturtle()
+for num in range(num_of_walls):
+    maze_painter.forward(wall_length)
+    maze_painter.left(90)
+    wall_length = wall_length + path_width
+    maze_painter.forward(10)
+    maze_painter.penup()
+    maze_painter.forward(10)
+    maze_painter.pendown()
+    maze_painter.forward(40)
+    maze_painter.left(90)
+    maze_painter.forward(2*path_width)
+    maze_painter.forward(2*-path_width)
+    maze_painter.right(90)
 
 
 
-x = 10
 
-for num in range(20):
-    trtl.forward(x)
-    trtl.left(90)
-    x = x + 10
-
-wn = trtl.Screen()
+wn = maze_painter.Screen()
 wn.mainloop()
